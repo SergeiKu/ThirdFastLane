acc10ee710373fa08f95228ba44fa02c704295f2
Sergey Kudryavtsev <sergey.developer@bk.ru>
2019-12-06 10:17:06 +0300

    Initial Commit

---
+++ ThirdFastLane/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ThirdFastLane
+//
+//  Created by Sergey Kudryavtsev on 06.12.2019.
+//  Copyright © 2019 SergeyKudryavtsev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
